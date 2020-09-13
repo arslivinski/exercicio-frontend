26f573cc976ebb0ca2cb49a354120b07efdc1504
Anderson Slivinski <11060804+arslivinski@users.noreply.github.com>
2020-09-13 17:32:41 -0300

    feat: create initial files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'core-js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function App() {
+  return null;
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));

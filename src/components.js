db7d18fdbb9148fd080f3385bfc04c9d667a29b7
Anderson Slivinski <11060804+arslivinski@users.noreply.github.com>
2020-09-13 23:36:33 -0300

    feat(component/social-media-icon): create component SocialMediaIcon

--- src/components.js
+++ src/components.js
@@ -1,1 +1,2 @@
 export * from './components/button.js';
+export * from './components/social-media-icon.js';
---
+++ src/components/social-media-icon.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './social-media-icon.css';
+
+/**
+ * @typedef SocialMediaIconProps
+ * @type {Object}
+ * @property {string} id
+ * @property {string} label
+ * @property {string} icon
+ * @property {string} url
+ */
+
+/**
+ * Small and round button with an icon that opens the provided link in a new tab
+ *
+ * @param {SocialMediaIconProps} props
+ * @returns {JSX.Element}
+ */
+function SocialMediaIcon(props) {
+  const { id, label, icon, url } = props;
+  return (
+    <a
+      id={`social-media-icons_${id}`}
+      className="jl-social-media-icon"
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      title={label}
+    >
+      <img src={icon} alt={`${label} logo`} />
+    </a>
+  );
+}
+
+SocialMediaIcon.propTypes = {
+  id: PropTypes.string,
+  label: PropTypes.string,
+  icon: PropTypes.string,
+  url: PropTypes.string,
+};
+
+export { SocialMediaIcon };
